0b51c5b3ad3f8fdb68c3f6c21722209ea2d7299d
Elijah Schow <elijah.schow@gmail.com>
2017-04-08 16:52:14 -0500

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+eval "$(ssh-agent -s)"
+KEY_ENC=.travis/sandbox.pem.enc
+KEY=.travis/sandbox.pem
+openssl aes-256-cbc -K $encrypted_c6c040438739_key -iv $encrypted_c6c040438739_iv -in $KEY_ENC -out $KEY -d
+rm $KEY_ENC
+chmod 600 $KEY
+mv $KEY_ENC ~/.ssh/id_rsa
+git remote add deploy $DEPLOY_REMOTE
+git push -fv deploy master
