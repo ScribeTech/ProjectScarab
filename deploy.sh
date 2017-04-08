e6c3584c0b947b0c0a99126994fdacada051ae69
Elijah Schow <elijah.schow@gmail.com>
2017-04-08 17:34:10 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,4 @@ openssl aes-256-cbc -K $encrypted_c6c040438739_key -iv $encrypted_c6c040438739_i
 rm $KEY_ENC
 chmod 600 $KEY
 mv $KEY_ENC ~/.ssh/id_rsa
-git remote add deploy $DEPLOY_REMOTE
-git checkout -b deploy-branch
-git push -f deploy deploy-branch
+scp . deploy@174.138.73.162:/var/www/ScribeTech/SurveyScribe
