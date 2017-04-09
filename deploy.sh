838b3f46b0a93a01506f0a4dd5631e3384115804
Elijah Schow <elijah.schow@gmail.com>
2017-04-08 19:21:09 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -x
 export KEY_ENC=".travis/sandbox.pem.enc"
 export KEY=".travis/sandbox.pem"
 openssl aes-256-cbc -K $encrypted_c6c040438739_key -iv $encrypted_c6c040438739_iv -in $KEY_ENC -out $KEY -d
