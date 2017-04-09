fa39b1f891cf7484d9a455b9bfd678e3e25cc580
Elijah Schow <elijah.schow@gmail.com>
2017-04-08 20:02:53 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -ex
+KEY_ENC=.travis/sandbox.pem.enc
+KEY=.travis/sandbox.pem
+openssl aes-256-cbc -K $encrypted_c6c040438739_key -iv $encrypted_c6c040438739_iv -in $KEY_ENC -out $KEY -d
+chmod 600 $KEY
+eval "$(ssh-agent -s)"
+ssh-add $KEY
+rm $KEY
 git remote add deploy $DEPLOY_REMOTE
 yes yes | git push -f deploy master
 exit 0
