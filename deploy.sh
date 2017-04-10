dd0ae67805f7e60e5478a350d2025ac8f01ca9d1
Elijah Schow <elijah.schow@gmail.com>
2017-04-10 17:47:16 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
 set -xve
-
-
+KEY_ENC=.travis/sandbox.pem.enc
+KEY=.travis/sandbox.pem
+openssl aes-256-cbc -K $encrypted_c6c040438739_key -iv $encrypted_c6c040438739_iv -in $KEY_ENC -out $KEY -d
+chmod 600 $KEY
+eval "$(ssh-agent -s)"
+ssh-add $KEY
+rm $KEY
+git remote add deploy ssh://server@174.138.73.162/home/server/sandbox.git
+git push --force --verbose deploy master
+exit 0
